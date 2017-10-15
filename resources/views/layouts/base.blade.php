04e20b6a92bd1501fef119400da08db073019945
Vince Kronlein <vkronlein@icloud.com>
2017-10-15 13:08:15 -0700

    initial commit
    Signed-off-by: Vince Kronlein <vkronlein@icloud.com>

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html ng-app="polr">
+<head>
+    <title>@section('title'){{env('APP_NAME')}}@show</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- Leave this for stats --}}
+    <meta name="generator" content="Polr {{env('POLR_VERSION')}}" />
+    @yield('meta')
+
+    {{-- Load Stylesheets --}}
+    @if (env('APP_STYLESHEET'))
+    <link rel="stylesheet" href="{{env('APP_STYLESHEET')}}">
+    @else
+    <link rel="stylesheet" href="/css/default-bootstrap.min.css">
+    @endif
+
+    <link href="/css/base.css" rel="stylesheet">
+    <link href="/css/toastr.min.css" rel="stylesheet">
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    @yield('css')
+</head>
+<body>
+    @include('snippets.navbar')
+    <div class="container-fluid">
+        <div class="content-div @if (!isset($no_div_padding)) content-div-padding @endif @if (isset($large)) jumbotron large-content-div @endif">
+            @yield('content')
+        </div>
+    </div>
+
+    {{-- Load JavaScript dependencies --}}
+    <script src="/js/constants.js"></script>
+    <script src="/js/jquery-1.11.3.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/angular.min.js"></script>
+    <script src="/js/toastr.min.js"></script>
+    <script src="/js/base.js"></script>
+
+    <script>
+    @if (Session::has('info'))
+        toastr["info"](`{{ str_replace('`', '\`', session('info')) }}`, "Info")
+    @endif
+    @if (Session::has('error'))
+        toastr["error"](`{{str_replace('`', '\`', session('error')) }}`, "Error")
+    @endif
+    @if (Session::has('warning'))
+        toastr["warning"](`{{ str_replace('`', '\`', session('warning')) }}`, "Warning")
+    @endif
+    @if (Session::has('success'))
+        toastr["success"](`{{ str_replace('`', '\`', session('success')) }}`, "Success")
+    @endif
+
+    @if (count($errors) > 0)
+        // Handle Lumen validation errors
+        @foreach ($errors->all() as $error)
+            toastr["error"](`{{ str_replace('`', '\`', $error) }}`, "Error")
+        @endforeach
+    @endif
+    </script>
+
+    @yield('js')
+</body>
+</html>
